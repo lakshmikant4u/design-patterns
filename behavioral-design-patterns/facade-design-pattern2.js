c33631ae1efd62cf90e8209fac2bac9c0b5d305e
Lakshmikant <lakshmikant4u@gmail.com>
2023-04-25 20:51:39 +0530

    Game Facade Design Pattern

---
+++ behavioral-design-patterns/facade-design-pattern2.js
@@ -0,0 +1,58 @@
+// Define the underlying complex subsystems
+const characterCreationSubsystem = () => {
+  const createCharacter = (name, race, class, ...) => {
+    // Complex code to create a character...
+    return character;
+  }
+
+  return { createCharacter };
+};
+
+const gameWorldSubsystem = () => {
+  const moveCharacter = (character, destination) => {
+    // Complex code to move a character...
+    return true;
+  }
+
+  const interactWithObject = (character, object) => {
+    // Complex code to interact with an object...
+    return true;
+  }
+
+  return { moveCharacter, interactWithObject };
+};
+
+// Define the facade that simplifies the API for the client
+const gameFacade = () => {
+  const characterCreation = characterCreationSubsystem();
+  const gameWorld = gameWorldSubsystem();
+
+  const createNewGame = (gameSettings) => {
+    // Complex code to initialize a new game...
+    return game;
+  }
+
+  const createNewCharacter = (name, race, class, ...) => {
+    const character = characterCreation.createCharacter(name, race, class, ...);
+    return character;
+  }
+
+  const moveCharacterTo = (character, destination) => {
+    const success = gameWorld.moveCharacter(character, destination);
+    return success;
+  }
+
+  const interactWith = (character, object) => {
+    const success = gameWorld.interactWithObject(character, object);
+    return success;
+  }
+
+  return { createNewGame, createNewCharacter, moveCharacterTo, interactWith };
+};
+
+// Example usage
+const game = gameFacade();
+
+const newCharacter = game.createNewCharacter("John", "Human", "Warrior");
+game.moveCharacterTo(newCharacter, "Castle");
+game.interactWith(newCharacter, "Treasure Chest");
